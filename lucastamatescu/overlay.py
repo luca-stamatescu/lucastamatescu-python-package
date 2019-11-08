6a356bf2a617708c7972481c53564893165d0850
luca-stamatescu <peppertherobot2017@gmail.com>
2019-11-08 17:12:27 +1100

    add gitignore and update overlay to remove need to determine screen size resolution

--- lucastamatescu/overlay.py
+++ lucastamatescu/overlay.py
@@ -1,18 +1,17 @@
 
 class video_overlay_class:
-    import screeninfo
     import cv2
     import numpy as np
-    print('started')
-    def __init__(self,overlay_image_name):
-        import screeninfo
+
+
+    def __init__(self,overlay_image_name,window_name):
         import cv2
         import numpy as np
-        cv2.namedWindow("Mask webcam", cv2.WND_PROP_FULLSCREEN)
-        cv2.setWindowProperty("Mask webcam", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-        # get the size of the screen
-        screen = screeninfo.get_monitors()[0]
-        width, height = screen.width, screen.height
+        cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        # # get the size of the screen
+        # screen = screeninfo.get_monitors()[0]
+        # width, height = screen.width, screen.height
         overlay_t_img = cv2.imread(overlay_image_name, -1)  # Load with transparency
         overlay_mask = overlay_t_img[:, :, 3:]  # And the alpha plane
         # Split out the transparency mask from the colour info
@@ -28,21 +27,20 @@ class video_overlay_class:
 
         self.__overlay_part = overlay_part
         self.__background_mask = background_mask
-        self.__height = height
-        self.__width = width
     def overlay_image(self,input_frame):
-        import screeninfo
         import cv2
         import numpy as np
+        print(input_frame.shape)
+        height, width, depth = input_frame.shape
         # Display the resulting frame
-        overlay_part = cv2.resize(self.__overlay_part, (640, 480))
-        background_mask = cv2.resize(self.__background_mask, (640, 480))
+        overlay_part = cv2.resize(self.__overlay_part, (width, height))
+        background_mask = cv2.resize(self.__background_mask, (width, height))
         # Logo set up for 1920x1080
         # Create a masked out face image, and masked out overlay
         # We convert the images to floating point in range 0.0 - 1.0
         temporary_frame = (input_frame * (1 / 255.0)) * (background_mask * (1 / 255.0))
         # And finally just add them together, and rescale it back to an 8bit integer image
-        temporary_frame = np.uint8(cv2.addWeighted(temporary_frame, 255.0, overlay_part, 255.0, 0.0))
-        output_frame = cv2.resize(temporary_frame, (self.__width, self.__height))
+        output_frame = np.uint8(cv2.addWeighted(temporary_frame, 255.0, overlay_part, 255.0, 0.0))
         return output_frame
+
 
